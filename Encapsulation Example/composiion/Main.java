2949fc679c034d4bd1eaaad0161b8bd8947cba6a
Nazir <nazirahmadsaidzada@gmail.com>
2018-04-11 18:51:04 +0800

    Add files via upload

--- Encapsulation Example/composiion/Main.java
+++ Encapsulation Example/composiion/Main.java
@@ -20,9 +20,39 @@ public class Main {
 //		p1.loseHealth(damage);
 //		System.out.println("Remaining health is: " +
 //		p1.remainingHealth());
+		//https://github.com/NazirAhmad123
+//		EnhancedPlayer p2 = new EnhancedPlayer("Ali",500, "sword");
+//		System.out.println("Remaining health is: " + p2.getHealth());
+//	
+	//Printer p = new Printer(50, true);
+//	Printer p = new Printer(50, false);
+//	
+//	int printThisAmount = p.printPages(4);
+//	System.out.println("Pages printed: "+printThisAmount
+//			+"\nNew total: " + p.getPagesPrinted());
 //		
-		EnhancedPlayer p2 = new EnhancedPlayer("Ali",500, "sword");
-		System.out.println("Remaining health is: " + p2.getHealth());
+//		
+//		
+//	printThisAmount = p.printPages(8);	
+//	System.out.println("Pages printed: "+printThisAmount
+//			+"\nNew total: " + p.getPagesPrinted());	
+//		
+		
+		
+		Dimension dimension = new Dimension(20,20,5);
+		Case theCase = new Case("BC234","Asus", dimension);
+		
+		
+		Monitor monitor = new Monitor("VNHG34","Dell",new Resolution(1200,1200));
+		
+		Motherboard motherboard = new Motherboard("CZ324","Acer");
+		
+		Computer myPc = new Computer(theCase,monitor, motherboard);
+		theCase.pressPowerButton();
+		monitor.drawPixelsAt(125, 123, "blue");
+		motherboard.loadProgram("Windows 8.1");
+		
+	
 	}
 
 }
